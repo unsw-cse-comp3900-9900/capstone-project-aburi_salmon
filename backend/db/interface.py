6c5c8034c50c1e53e9d7c3c3a0f9ba9cbbd3b220
noahkessler <noahkessler@outlook.com>
2020-03-16 16:11:15 +1100

    Refactor interface file, so that we don't have to duplicate code all of the time

--- backend/db/interface.py
+++ backend/db/interface.py
@@ -40,65 +40,11 @@ class DB:
         self.__conn.commit()
         return True
 
-    def return_number(self, number):
+    def __insert(self, insert, params):
         c = self.__conn.cursor()
 
         try:
-            c.execute("SELECT %s AS example;", (number,))
-        except Exception as e:
-            c.execute("ROLLBACK")
-            self.__conn.commit()
-            print(e)
-            c.close()
-            return None
-
-        rows = c.fetchall()
-        if len(rows) == 0:
-            c.close()
-            return None
-
-        c.close()
-        return rows[0][0]
-
-
-    # NOTE: Take these functions as example since you might have different implementations
-    
-    def login(self, username, password):
-        rows = self.__query("SELECT password FROM staff WHERE username = %s;", [username])
-        if (rows == None):
-            return False
-
-        # This compares two strings to prevent timing attack
-        result = hmac.compare_digest(rows[0][0], password)
-        return result
-
-    def validate_key(self, key):
-        c = self.__conn.cursor()
-
-        try:
-            # This might be different, depending on your table and column name
-            c.execute("SELECT id FROM staff_registration WHERE registration_key = %s;", (key,))
-        except Exception as e:
-            c.execute("ROLLBACK")
-            self.__conn.commit()
-            print(e)
-            c.close()
-            return False
-
-        rows = c.fetchall()
-        if len(rows) == 0:
-            c.close()
-            return False
-
-        c.close()
-        return rows[0][0]
-        
-    def register(self, username, password, name, staff_type_id):
-        c = self.__conn.cursor()
-
-        try:
-            c.execute("INSERT INTO staff (username, password, name, staff_type_id) VALUES (%s, %s, %s, %s);",
-                      (username, password, name, staff_type_id))
+            c.execute(insert, params)
         except Exception as e:
             c.execute("ROLLBACK")
             self.__conn.commit()
@@ -109,48 +55,52 @@ class DB:
 
         c.close()
         self.__conn.commit()
+
+
+    def return_number(self, number):
+        rows = self.__query("SELECT %s AS example;", [number])
+        if (not rows):
+            return None
+    
+        return rows[0][0]
+
+
+    # NOTE: Take these functions as example since you might have different implementations
+    
+    def login(self, username, password):
+        rows = self.__query("SELECT password FROM staff WHERE username = %s;", [username])
+        if (not rows):
+            return False
+
+        # This compares two strings to prevent timing attack
+        result = hmac.compare_digest(rows[0][0], password)
+        return result
+
+    def validate_key(self, key):
+        rows = self.__query("SELECT id FROM staff_registration WHERE registration_key = %s;", [key])
+        if (rows == None):
+            return False
+
+        return rows[0][0]
+        
+    def register(self, username, password, name, staff_type_id):
+        self.__insert("INSERT INTO staff (username, password, name, staff_type_id) VALUES (%s, %s, %s, %s);",
+                      [username, password, name, staff_type_id])
         return True
 
     def available_username(self, username):
-        c = self.__conn.cursor()
+        rows = self.__query("SELECT COUNT(*) FROM staff WHERE username = %s;", [username])
+        if (not rows):
+            return False
 
-        try:
-            c.execute("SELECT COUNT(*) FROM staff WHERE username = %s;", (username,))
-        except Exception as e:
-            c.execute("ROLLBACK")
-            self.__conn.commit()
-            print(e)
-            c.close()
-            return None
-
-        rows = c.fetchall()
-        if len(rows) == 0:
-            c.close()
-            return None
         rlen = rows[0][0]
-
-        c.close()
         return (rlen == 0)
 
     def get_profile(self, username):
-        c = self.__conn.cursor()
-
-        try:
-            # This might be different, depending on your table and column name
-            c.execute("SELECT username, name, staff_type_id FROM staff WHERE username = %s;", (username,))
-        except Exception as e:
-            c.execute("ROLLBACK")
-            self.__conn.commit()
-            print(e)
-            c.close()
+        rows = self.__query("SELECT username, name, staff_type_id FROM staff WHERE username = %s;", [username])    
+        if (not rows):
             return None
 
-        rows = c.fetchall()
-        if len(rows) == 0:
-            c.close()
-            return None
-
-        c.close()
         return dict(
             username=rows[0][0],
             name=rows[0][1],
@@ -159,6 +109,10 @@ class DB:
 
     def get_ordered_items(self):
         rows = self.__query('SELECT * FROM item_order WHERE status_id < %s', [3])
+
+        if (not rows):
+            return None
+
         orders = [{
             'id': row[0],
             'item_id': row[1],
