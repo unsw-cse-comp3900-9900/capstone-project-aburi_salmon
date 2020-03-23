c4309ff3e6a4dabea6466383d34ccae98724df9d
Kaveh Faghani <kavehfaghani@hotmail.com>
2020-03-23 21:30:53 +1100

    edited function

--- backend/db/interface.py
+++ backend/db/interface.py
@@ -367,7 +367,7 @@ class DB:
         return self.__delete("DELETE FROM item_order WHERE order_id = %s AND item_id = %s", [order_id, item_id])
 
 
-    def get_ordered_items_customer(self, status):
+    def get_order_list(self, status):
 
         rows = self.__query('SELECT item.name, io.quantity, item.price FROM item_order io JOIN item ON io.id = item.id WHERE io.status_id == %s', [status])
 
